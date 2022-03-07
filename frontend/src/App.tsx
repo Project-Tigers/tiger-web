5846945f3a5746a3acf22476630deb380b43fbbf
Quinn Develen <develen.1@osu.edu>
2022-03-07 18:40:16 -0500

    Co-authored-by: Danny Mach <dannymach@users.noreply.github.com> Co-authored-by: Navya Kovvuri <professortiger@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./Home/Home";
 import Product from "./Products/Products"
 import Orders from './Orders/Orders'
 import OrderDetail from './Orders/OrderDetails'
+import Payment from './Payment/Payment'
 import './App.css';
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
               <Link to="/">Home</Link>
               <Link to="/catalog">Catalog</Link>
               <Link to="/orders">Orders</Link>
+              <Link to="/payment">Payment</Link>
             </div>
           </header>
           <aside className="sidebar">
@@ -52,6 +54,7 @@ function App() {
               <Route path="/catalog" element={<Product />} />
               <Route path="orders" element={<Orders />}></Route>
               <Route path="/order/:id" element={<OrderDetail />}></Route>
+              <Route path="payment" element={<Payment />}></Route>
             </Routes>
           </main>
           <footer className="footer">&copy; 2022 Tigers </footer>
